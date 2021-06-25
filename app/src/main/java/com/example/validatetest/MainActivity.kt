bb98c33d6e033758bd4f4530e3712c780a0649b2
kamioka24 <keee2404@gmail.com>
2021-06-25 12:52:17 +0900

    [Finish]

--- app/src/main/java/com/example/validatetest/MainActivity.kt
+++ app/src/main/java/com/example/validatetest/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.validatetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edText = findViewById<EditText>(R.id.edText)
+        val tvText = findViewById<TextView>(R.id.tvText)
+        val enter = findViewById<Button>(R.id.enter)
+
+        enter.setOnClickListener {
+            val text = edText.text.toString()
+            if (text == "") {
+                edText.error = "名前を入力してください"
+            } else {
+                tvText.text = text
+            }
+        }
     }
 }
